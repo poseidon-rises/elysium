8afbbcc9de0e91b48e08c132c6401602a5de57eb
Cool-Game-Dev <softwaredevelopment.stingray177@passinbox.com>
2025-08-10 13:01:33 -0500

    Add ns function

--- modules/home/shells/shells/fish/init-last.fish
+++ modules/home/shells/shells/fish/init-last.fish
@@ -1,1 +1,10 @@
+# Allows quick creation of nix shells. For example, 'ns hello cowsay' will
+# create shell with the hello package and the cowsay package
+function ns
+    set -l transformed
+    for pkg in $argv
+        set transformed $transformed "nixpkgs#$pkg"
+    end
+    nix shell $transformed
+end
 
