2268dc34f0b06601fc45c22c4af46f3224e844b1
Cool-Game-Dev <softwaredevelopment.stingray177@passinbox.com>
2025-08-10 13:02:10 -0500

    Add tagstudio

---
+++ modules/home/programs/utilities/tagstudio.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+let
+  cfg' = config.elysium.programs.utilities;
+  cfg = cfg'.tagstudio;
+in
+{
+  options.elysium.programs.utilities.tagstudio.enable = lib.mkEnableOption "TagStudio" // {
+    default = cfg'.enable;
+  };
+
+  config = lib.mkIf cfg.enable {
+    home.packages = [
+      #			inputs.tagstudio.packages.${pkgs.system}.default
+    ];
+  };
+}
