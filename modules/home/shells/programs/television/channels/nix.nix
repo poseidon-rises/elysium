1950b9bcaddad9ba9efff042f2e3b4b6fac4448a
Cool-Game-Dev <softwaredevelopment.stingray177@passinbox.com>
2025-08-13 14:33:59 -0500

    Add nix-search-tv settings

--- modules/home/shells/programs/television/channels/nix.nix
+++ modules/home/shells/programs/television/channels/nix.nix
@@ -16,7 +16,19 @@ in
     };
 
   config.programs = lib.mkIf (cfg'.enable && cfg.enable) {
-    nix-search-tv.enable = true;
+    nix-search-tv = {
+      enable = true;
+      settings = {
+        indexes = [
+          "home-manager"
+          "nixos"
+          "nixpkgs"
+          "nur"
+        ];
+
+        experimental.render_docs_indexes.nvf = "https://notashelf.github.io/nvf/options.html";
+      };
+    };
     television.channels.nix-search-tv.metadata.name = lib.mkForce "nix";
   };
 }
