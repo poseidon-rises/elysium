74f13a6ae1f5b610344996affe91ee18a2f8358f
Poseidon <softwaredevelopment.stingray177@passinbox.com>
2025-08-15 17:47:04 -0500

    Fix ripgrep module

---
+++ modules/home/shells/programs/ripgrep/default.nix
@@ -0,0 +1,14 @@
+{ config, lib, ... }:
+
+let
+	cfg' = config.elysium.shells.programs;
+	cfg = cfg'.ripgrep;
+in {
+	options.elysium.shells.programs.ripgrep.enable = lib.mkEnableOption "ripgrep" // {
+		default = cfg'.enableUseful;
+	};
+
+	config = lib.mkIf cfg.enable {
+		programs.ripgrep.enable = true;
+	};
+}
