2bea28f97a496b233de9cb20c683ddaf89509c97
Poseidon <softwaredevelopment.stingray177@passinbox.com>
2025-08-15 17:47:30 -0500

    Add audio module

---
+++ modules/core/management/audio.nix
@@ -0,0 +1,18 @@
+{ config, lib, ... }:
+
+let
+	cfg = config.elysium.management.audio;
+in {
+	options.elysium.management.audio.enable = lib.mkEnableOption "audio" // {
+		default = lib.elem "Graphical" config.chaos.aspects;
+	};
+
+	config = lib.mkIf cfg.enable {
+		services.pipewire = {
+			enable = true;
+			pulse.enable = true;
+			jack.enable = true;
+			alsa.enable = true;
+		};
+	};
+}
