b1270effa2925ccd487bbcfe5c310a3f756e7805
Poseidon <softwaredevelopment.stingray177@passinbox.com>
2025-08-21 16:40:01 -0400

    Add firewall.allowPing and allow kdeconnect traffic conditionally

--- modules/core/networking/firewall.nix
+++ modules/core/networking/firewall.nix
@@ -1,12 +1,32 @@
-{ ... }:
+{ config, lib, ... }:
 
+let
+  cfg = config.elysium.networking.firewall;
+  anyUserKdeConnect = lib.elysium.anyUserEnables [
+    "elysium"
+    "programs"
+    "internet"
+    "kdeconnect"
+    "enable"
+  ] config;
+in
 {
-  networking.firewall = {
-    allowedUDPPortRanges = [
-      {
+  options.elysium.networking.firewall = {
+    enable = lib.mkEnableOption "firewall" // {
+      default = true;
+    };
+    allowPing = lib.mkEnableOption "allow pings" // {
+      default = !lib.elem "Server" config.chaos.aspects;
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    networking.firewall = {
+      enable = true;
+      inherit (cfg) allowPing;
+      allowedUDPPortRanges = lib.optional anyUserKdeConnect {
         from = 1714;
         to = 1764;
-      }
-    ];
+      };
+    };
   };
 }
