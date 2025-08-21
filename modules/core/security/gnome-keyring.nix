3f278441694d778793e42fd2e0e4c391a7b4a648
Poseidon <softwaredevelopment.stingray177@passinbox.com>
2025-08-21 16:38:08 -0400

    Move gnome-keyring to elysium.security

---
+++ modules/core/security/gnome-keyring.nix
@@ -0,0 +1,15 @@
+{ config, lib, ... }:
+
+let
+  cfg = config.elysium.security.gnome-keyring;
+in
+{
+  options.elysium.security.gnome-keyring.enable = lib.mkEnableOption "Gnome Keyring" // {
+    default = lib.elem "Graphical" config.chaos.aspects;
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.gnome.gnome-keyring.enable = true;
+    security.pam.services.login.enableGnomeKeyring = true;
+  };
+}
