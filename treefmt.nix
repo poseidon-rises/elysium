65a4f104875057bdf482e83eeaa9fa6cac40523e
Poseidon <softwaredevelopment.stingray177@passinbox.com>
2025-08-21 21:41:12 -0400

    Clean up treefmt.nix

--- treefmt.nix
+++ treefmt.nix
@@ -1,13 +1,42 @@
-{ ... }:
-
 {
   # Nix
   programs = {
+    # Formatters
     nixfmt.enable = true;
+
+    # Linters
+    statix.enable = true;
     deadnix.enable = true;
   };
 
-  # Config formats
-  programs.yamlfmt.enable = true;
-  programs.jsonfmt.enable = true;
+  # CSS, Markdown, YAML, and JSON
+  programs = {
+    # Formatters
+    deno.enable = true;
+  };
+
+  # TOML
+  programs = {
+    # Formatters
+    taplo.enable = true;
+    toml-sort.enable = true;
+  };
+
+  # Bash/sh
+  programs = {
+    # Formatters
+    shellcheck = {
+      enable = true;
+      excludes = [ ".envrc" ];
+    };
+
+    # Linters
+    shfmt.enable = true;
+  };
+
+  # Fish
+  programs = {
+    # Formatters
+    fish_indent.enable = true;
+  };
 }
