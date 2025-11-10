c10261827c6ac5fbc9f3ba4b5e5aaa46a882b547
Poseidon <softwaredevelopment.stingray177@passinbox.com>
2025-11-10 14:01:53 -0600

    shell: Add quickshell development support

--- shell.nix
+++ shell.nix
@@ -4,6 +4,9 @@ pkgs.mkShell {
   nativeBuildInputs = [
     pkgs.nh
     pkgs.just
+    pkgs.kdePackages.qtdeclarative
+    pkgs.quickshell
   ];
   buildInputs = checks.pre-commit-check.enabledPackages;
+  QMLLS_BUILD_DIRS = "${pkgs.kdePackages.qtdeclarative}/lib/qt-6/qml/:${pkgs.quickshell}/lib/qt-6/qml/";
 }
