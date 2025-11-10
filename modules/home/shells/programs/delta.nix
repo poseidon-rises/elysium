84b26a2635aa379004bfb6ac90d5707668155041
Poseidon <softwaredevelopment.stingray177@passinbox.com>
2025-11-10 13:58:53 -0600

    elysium/home/delta: init

---
+++ modules/home/shells/programs/delta.nix
@@ -0,0 +1,19 @@
+{ config, lib, ... }:
+
+let
+  cfg' = config.elysium.shells.programs;
+  cfg = cfg'.delta;
+  gitCfg = config.elysium.development.tools.git;
+in
+{
+  options.elysium.shells.programs.delta.enable = lib.mkEnableOption "Delta" // {
+    default = cfg'.enableUseful || gitCfg.enable;
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.delta = {
+      enable = true;
+      enableGitIntegration = gitCfg.enable;
+    };
+  };
+}
