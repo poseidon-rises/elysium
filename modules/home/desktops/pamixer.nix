3c8d35f6ebbe594d94f154e656d830c4ffe8f8ef
Poseidon <softwaredevelopment.stingray177@passinbox.com>
2025-11-10 13:54:22 -0600

    elysium/home/pamixer: Enable by default

--- modules/home/desktops/pamixer.nix
+++ modules/home/desktops/pamixer.nix
@@ -10,7 +10,9 @@ let
   cfg = cfg'.pamixer;
 in
 {
-  options.elysium.desktops.pamixer.enable = lib.mkEnableOption "pamixer";
+  options.elysium.desktops.pamixer.enable = lib.mkEnableOption "pamixer" // {
+    default = cfg'.enable;
+  };
 
   config = lib.mkIf (cfg'.enable && cfg.enable) {
     home.packages = [ pkgs.pamixer ];
