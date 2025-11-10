e38ca2e3f1012c031fecf94529ef031d8d095e10
Poseidon <softwaredevelopment.stingray177@passinbox.com>
2025-11-10 13:55:52 -0600

    elysium/home/mpd: Add fifo

--- modules/home/services/mpd/default.nix
+++ modules/home/services/mpd/default.nix
@@ -15,7 +15,21 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    services.mpd.enable = true;
+    services.mpd = {
+      enable = true;
+      extraConfig = ''
+        audio_output {
+        	type      "pipewire"
+        	name      "Pipewire Sound Server"
+        } 
+        audio_output {
+        	type   "fifo"
+        	name   "mpd_fifo"
+        	path   "/tmp/mpd.fifo"
+        	format "44100:16:2"
+        }
+      '';
+    };
     services.mpd-mpris.enable = true;
   };
 }
