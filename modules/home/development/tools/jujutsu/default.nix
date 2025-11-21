224e9bcc459cc816a10d3ab113ea0c6255c41174
Poseidon <softwaredevelopment.stingray177@passinbox.com>
2025-11-21 12:30:37 -0600

    home/development/jujutsu: init

---
+++ modules/home/development/tools/jujutsu/default.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+let
+  cfg' = config.elysium.development.tools;
+  cfg = cfg'.jujutsu;
+in
+{
+	imports = lib.elysium.scanPaths ./.;
+		options.elysium.development.tools.jujutsu.enable = lib.mkEnableOption "jj VSC system" // {
+    default = cfg'.enable;
+  };
+
+  config = lib.mkIf cfg.enable {
+		programs.jujutsu.enable = true;
+  };
+}
